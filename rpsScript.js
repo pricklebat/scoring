1e3d979b60c2386381eebcadad12737db546fa6e
pricklebat <67910319+pricklebat@users.noreply.github.com>
2021-07-29 00:28:56 +0900

    Update rpsScript.js

--- rpsScript.js
+++ rpsScript.js
@@ -23,26 +23,6 @@
      document.getElementById("hand2").innerHTML = ('<img class="who2" src="' + myImages2[rnd2] + '">');
     }
 
-function startGame() {
-    var myImages1 = new Array();
-    myImages1[1] = "https://res.cloudinary.com/itsellej/image/upload/v1533400140/rock-paper-scissors/rock.png";
-    myImages1[2] = "https://res.cloudinary.com/itsellej/image/upload/v1533400143/rock-paper-scissors/paper.png";
-    myImages1[3] = "https://res.cloudinary.com/itsellej/image/upload/v1533400144/rock-paper-scissors/scissors.png";
-    var rnd1 = Math.floor(Math.random() * myImages1.length);
-    if (rnd1 == 0) {
-            rnd1 = 1;
-    }
-  var myImages2 = new Array();
-    myImages2[1] = "https://res.cloudinary.com/itsellej/image/upload/v1533400140/rock-paper-scissors/rock.png";
-    myImages2[2] = "https://res.cloudinary.com/itsellej/image/upload/v1533400143/rock-paper-scissors/paper.png";
-    myImages2[3] = "https://res.cloudinary.com/itsellej/image/upload/v1533400144/rock-paper-scissors/scissors.png";
-    var rnd2 = Math.floor(Math.random() * myImages2.length);
-    if (rnd2 == 0) {
-            rnd2 = 1;
-    }
-     document.getElementById("hand1").innerHTML = ('<img class="who1" src="' + myImages1[rnd1] + '">');
-     document.getElementById("hand2").innerHTML = ('<img class="who2" src="' + myImages2[rnd2] + '">');
-}
 
  function leaderSelectA() {
     const input = document.getElementById('team1').value;
